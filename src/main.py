84927290ab239d6f26b686fff63b9742f021d3cb
Ricardo Lence <lencericardo@gmail.com>
2021-01-13 16:31:31 +0000

    ahora si sirviendo imagenes, el anterior es para guardar imagenes

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from os.path import join, dirname, realpath
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, send_from_directory, send_file
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
@@ -17,7 +17,7 @@ from werkzeug.datastructures import ImmutableMultiDict
 
 #from models import Person
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./img")
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -25,6 +25,7 @@ MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
+HOST = 'https://3000-d4ff11bd-ce8e-4f2e-9639-7fa4dbf90e0d.ws-eu03.gitpod.io/'
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -35,6 +36,14 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
+#send img
+@app.route('/<filename>', methods=['GET'])
+def send_img(filename):
+    print(filename)
+    path = os.path.join('./src/img')
+    print(path)
+    return  send_file("./img/" + filename)
 
 @app.route('/user/register', methods=['POST'])
 def register_user():
@@ -81,18 +90,17 @@ def login_user():
 
 @app.route('/post', methods=['POST'])
 def create_post():
-    data = dict(request.form)
 
+    data = dict(request.form)
+    
     f = request.files['file']
     filename = secure_filename(f.filename)
     f.save(os.path.join('./src/img',filename))
-    
-    urlImg = 'https://blogthinkbig.com/wp-content/uploads/sites/4/2019/03/Python-Example-Logo-e1555498232969.jpg?fit=1500%2C663' 
-    #new_post = Post(urlImg, body['text'], body['user_id'])
-    #db.session.add(new_post)
-    #db.session.commit()
-    #rint(new_post)
-    return jsonify("post created"), 201
+    img_url = HOST + filename
+    new_post = Post(img_url, data['text'], data['user_id'])
+    db.session.add(new_post)
+    db.session.commit()
+    return jsonify(new_post.serialize()), 201
 
 
 @app.route('/post/<id>', methods=['GET'])
