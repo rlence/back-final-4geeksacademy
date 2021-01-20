6515aeecad2aaecfaa937d4a834496e1023ce95f
Ricardo Lence <lencericardo@gmail.com>
2021-01-20 18:37:12 +0000

    quitando print

--- src/jwt_auth.py
+++ src/jwt_auth.py
@@ -2,8 +2,6 @@ import jwt
 import datetime
 
 def encode_token(user, key):
-    print(user)
-    print(key)
     return jwt.encode({"user":user, "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=1)}, key )
 
 def decode_token(token, key):
